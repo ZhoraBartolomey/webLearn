93cfbfed3b31061a32c894907e8122f0670b2a08
George Bartolomey <zhora.bartolomey@mail.ru>
2020-01-27 11:26:29 +0300

    Fix bug

--- controllers/all.js
+++ controllers/all.js
@@ -5,5 +5,5 @@ const express = require('express');
  * @param {express.Express} app 
  */
 module.exports = function (app) {
-    app.use(express.static(__dirname + '/../docs'));
+    app.use('/webLearn', express.static(__dirname + '/..d/ocs'));
 }

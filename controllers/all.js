fc29eca87a7ed4163f591cbbb9e4fe070df4bf07
George Bartolomey <zhora.bartolomey@mail.ru>
2020-01-27 11:42:31 +0300

    Add ads

--- controllers/all.js
+++ controllers/all.js
@@ -5,5 +5,5 @@ const express = require('express');
  * @param {express.Express} app 
  */
 module.exports = function (app) {
-    app.use('/webLearn', express.static(__dirname + '/..d/ocs'));
+    app.use('/webLearn', express.static(__dirname + '/../docs'));
 }
